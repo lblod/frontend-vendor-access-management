3faaf37f808741746580dd4f7a47d1723024be0c
achraf <achrafatauil@hotmail.com>
2020-09-28 11:31:18 +0200

    Fix typo in name bestuurseenheid

--- app/models/bestuurseenheid,.js
+++
@@ -1,8 +0,0 @@
-import Model, { attr, belongsTo } from '@ember-data/model';
-
-export default class BestuurseenheidModel extends Model {
-  @attr uri;
-  @attr naam;
-  @attr alternatieveNaam
-  
-}
