9e4eeb0cd19498d61a759f6c87f5fa6d44a5b5f4
achraf <achrafatauil@hotmail.com>
2020-09-29 11:19:55 +0200

    Add custom inflector rule for vendor & bestuurseenheid

--- app/initializers/custom-inflector-rules.js
+++ app/initializers/custom-inflector-rules.js
@@ -3,7 +3,8 @@ import Inflector from 'ember-inflector';
 export function initialize(/* application */) {
   const inflector = Inflector.inflector;
 
-  inflector.plural(/eid$/,'eden');
+  inflector.irregular("bestuurseenheid",'bestuurseenheden');
+  inflector.irregular("vendor",'vendors');
 }
 
 export default {
