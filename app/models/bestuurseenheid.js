5c6f40b323365249f38a0254092e06d7b7c82463
achraf <achrafatauil@hotmail.com>
2020-09-29 17:49:02 +0200

    Show classificatie type for each appropriate bestuurseenheid

--- app/models/bestuurseenheid.js
+++ app/models/bestuurseenheid.js
@@ -1,7 +1,8 @@
-import Model, { attr } from '@ember-data/model';
+import Model, { attr, belongsTo } from '@ember-data/model';
 
 export default class BestuurseenheidModel extends Model {
   @attr('string') uri;
   @attr("string") naam;
   @attr('string') alternatieveNaam;
+  @belongsTo('bestuurseenheid-classificatie-code') classificatie;
 }
--- app/routes/vendors/subject.js
+++ app/routes/vendors/subject.js
@@ -2,7 +2,7 @@ import Route from '@ember/routing/route';
 
 export default class VendorsSubjectRoute extends Route {
   model(params){
-    return this.store.findRecord('vendor',  params.id ,{ include: "can-act-on-behalf" }
+    return this.store.findRecord('vendor',  params.id ,{ include: ["can-act-on-behalf" ,"classificatie" ] }
     )
   }
 }
