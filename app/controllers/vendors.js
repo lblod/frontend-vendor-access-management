b8709e78b1bf4e3ef50bd050321d445dce8d5308
achraf <achrafatauil@hotmail.com>
2020-09-29 11:20:25 +0200

    Show vendor names & uri in table using ember-data-table

--- app/controllers/vendors.js
+++ app/controllers/vendors.js
@@ -1,5 +1,8 @@
 import Controller from '@ember/controller';
+import { tracked } from '@glimmer/tracking';
 
 export default class VendorsController extends Controller {
-
+  @tracked sort = '';
+  @tracked page = 0;
+  @tracked size = 20;
 }
