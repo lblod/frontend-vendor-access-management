b322a01806fa5e88e7dd66c60dbf624ed0d3e3e2
achraf <achrafatauil@hotmail.com>
2020-09-30 12:28:59 +0200

    Add code to update vendor relationship (not working)

---
+++ app/controllers/vendors/subject.js
@@ -0,0 +1,18 @@
+import Controller from '@ember/controller';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
+ 
+
+export default class VendorsSubjectController extends Controller {
+  @tracked page = 0
+  @tracked size = 10
+
+  @action async addBestuurseenheid(bestuurseenheid, vendor){
+    let targetVendor = this.store.peekRecord('vendor', vendor.id)
+    let bestuurseenheidRelation = this.store.peekRecord('bestuurseenheid', bestuurseenheid.id)
+    let canActOnBehalf = await vendor.canActOnBehalf
+    canActOnBehalf.pushObject(bestuurseenheidRelation)
+    targetVendor.save()
+  }
+
+}
