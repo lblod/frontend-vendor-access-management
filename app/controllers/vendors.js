186846889a6cae4f4e0277c25e218c482ff69276
achraf <achrafatauil@hotmail.com>
2020-09-30 12:29:12 +0200

    remove vendors.js controller

--- app/controllers/vendors.js
+++
@@ -1,8 +0,0 @@
-import Controller from '@ember/controller';
-import { tracked } from '@glimmer/tracking';
-
-export default class VendorsController extends Controller {
-  @tracked sort = '';
-  @tracked page = 0;
-  @tracked size = 20;
-}
