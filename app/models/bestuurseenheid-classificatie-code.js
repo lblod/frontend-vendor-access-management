7ef2fe684b9d3ad67b271aa0b25563f36bddc4e5
achraf <achrafatauil@hotmail.com>
2020-09-30 12:26:59 +0200

    Remove unused scopenote

--- app/models/bestuurseenheid-classificatie-code.js
+++ app/models/bestuurseenheid-classificatie-code.js
@@ -2,5 +2,4 @@ import Model, { attr } from '@ember-data/model';
 
 export default class BestuurseeheidClassificatieCodeModel extends Model {
   @attr('string') label;
-  @attr('string') scopeNote;
 }
