0ace01d5e539c8205b5653d5d1d4fdde519cd611
achraf <achrafatauil@hotmail.com>
2020-10-01 17:28:48 +0200

    Fetch vendor detail using ember-data-table method

--- app/routes/vendors/index.js
+++ app/routes/vendors/index.js
@@ -1,10 +1,7 @@
 import Route from '@ember/routing/route';
 import DataTableRouteMixin from 'ember-data-table/mixins/route';
+
 export default Route.extend(DataTableRouteMixin, {
-  modelName: 'vendors',
+  modelName: 'vendor'
   
-  model(params){
-    console.log(params)
-    return this.store.findAll('vendor')
-  }
 });
