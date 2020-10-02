7a2d2d16e5bfca106bccff5e784aa6b851f6b9a0
achraf <achrafatauil@hotmail.com>
2020-10-02 13:25:41 +0200

    Replace data-table "add-list" by modal (see description)
    - 2 data tables on one page gives issues since changing page on one data table would also have effect on the other as they share the same query params. Doubling query params does not feel like an elegant solution so taking a different approach with power-select inside a modal is much cleaner

--- app/components/bestuurseenheid-toevoegen.js
+++ app/components/bestuurseenheid-toevoegen.js
@@ -1,12 +1,19 @@
 import Component from '@glimmer/component';
-
 import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
+import { inject as service } from '@ember/service';
+import { A } from '@ember/array';
 
 
 export default class BestuurseenheidToevoegenComponent extends Component {
+  @service store;
+  @tracked bestuurseenhedenLijst = A([])
 
-  @action debug(){
-    console.log("debug")
+  @action async appendBestuurseenheid(eenheid){
+    console.log(eenheid)
+    let targetBestuurseenheid = await this.store.peekRecord('bestuurseenheid', eenheid.id)
+    this.bestuurseenhedenLijst.pushObject(targetBestuurseenheid)
+    console.log(this.bestuurseenhedenLijst)
   }
 
 }
--- app/controllers/vendors/subject.js
+++ app/controllers/vendors/subject.js
@@ -2,28 +2,32 @@ import Controller from '@ember/controller';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 import { task } from 'ember-concurrency';
+import { A } from '@ember/array';
+
  
 
 export default class VendorsSubjectController extends Controller {
   @tracked sort = "name"
   @tracked page = 0
   @tracked size = 10
+  @tracked bestuurseenhedenLijst = A([])
 
   @action async patchVendor(bestuurseenheid, vendor, todo){
     // Get vendor, bestuurseenheid & relationship array
     let targetVendor = this.store.peekRecord('vendor', vendor.id)
-    let targetBestuurseenheid = this.store.peekRecord('bestuurseenheid', bestuurseenheid.id)
-    let relationship = await vendor.canActOnBehalfOf
+
+    let relationship = await targetVendor.canActOnBehalfOf
 
     // Action to be performed based on argument
     if (todo == "delete"){
+      let targetBestuurseenheid = this.store.peekRecord('bestuurseenheid', bestuurseenheid.id)
       relationship.removeObject(targetBestuurseenheid)
     }
 
     if (todo == "add") {
-      relationship.pushObject(targetBestuurseenheid)
+      relationship.pushObjects(this.bestuurseenhedenLijst)
     }
-
+    
     // PATCH /
     targetVendor.save()
   }
@@ -33,5 +37,12 @@ export default class VendorsSubjectController extends Controller {
     let queryParams = {'filter[naam]': term};
     return this.store.query('bestuurseenheid', queryParams);
   })) searchBestuursType;
+
+  @action async appendBestuurseenheid(eenheid){
+    let targetBestuurseenheid = await this.store.peekRecord('bestuurseenheid', eenheid.id)
+    this.bestuurseenhedenLijst.pushObject(targetBestuurseenheid)
+  }
+
+
 
 }
