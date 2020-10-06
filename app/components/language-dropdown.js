1a7e8662fe7aba4e15258fa0e634bd2d4165bf97
achraf <achrafatauil@hotmail.com>
2020-10-06 14:02:00 +0200

    Add language dropdown ( for now only 2 buttons)
    The dropdown buttons action does not work because it closes before the action is completed. Need to find fix for that

---
+++ app/components/language-dropdown.js
@@ -0,0 +1,12 @@
+import Component from '@glimmer/component';
+import { inject as service } from '@ember/service';
+import { action } from '@ember/object';
+
+export default class LanguageDropdownComponent extends Component {
+  @service("intl") intl;
+
+  @action changeLanguage(lang){
+    console.log(this.intl.set("locale", lang))
+
+  }
+}
