21c318c1cd23a393e36e4957e948bd218a15e772
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2020-10-14 11:26:36 +0200

    Improvement: better to filter on URI, a label can change

--- app/routes/login.js
+++ app/routes/login.js
@@ -15,7 +15,9 @@ export default class LoginRoute extends Route {
     const filter = {
       provider: 'https://github.com/lblod/mock-login-service',
       gebruiker: {
-        bestuurseenheden: 'Agentschap Binnenlands Bestuur'
+        bestuurseenheden: {
+          ':uri:': 'http://data.lblod.info/id/bestuurseenheden/141d9d6b-54af-4d17-b313-8d1c30bc3f5b'
+        }
       }
     };
     return this.store.query('account', {
