15ce636ec8f04a70c526fc3c5dedcd94fb37ef59
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2020-10-14 11:30:09 +0200

    linting: fix missing ';'

--- app/routes/vendors/subject.js
+++ app/routes/vendors/subject.js
@@ -2,9 +2,9 @@ import Route from '@ember/routing/route';
 
 export default class VendorsSubjectRoute extends Route {
   model(params){
-    return  Ember.RSVP.hash({
+    return Ember.RSVP.hash({
       vendor: this.store.findRecord('vendor',  params.id , { include: "can-act-on-behalf-of" }),
       bestuurseenheden: this.store.findAll('bestuurseenheid')
-    })
+    });
   }
 }
