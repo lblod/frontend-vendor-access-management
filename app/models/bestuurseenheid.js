855eb5252f8a5198fbcfdcb3626cc1367d0de769
achraf <achrafatauil@hotmail.com>
2020-10-16 11:56:42 +0200

    Add hasMany vendors relation to bestuurseenheid

--- app/models/bestuurseenheid.js
+++ app/models/bestuurseenheid.js
@@ -1,8 +1,9 @@
-import Model, { attr, belongsTo } from '@ember-data/model';
+import Model, { attr, belongsTo, hasMany  } from '@ember-data/model';
 
 export default class BestuurseenheidModel extends Model {
   @attr('string') uri;
   @attr("string") naam;
   @attr('string') alternatieveNaam;
   @belongsTo('bestuurseenheid-classificatie-code') classificatie;
+  @hasMany('vendor') vendors;
 }
