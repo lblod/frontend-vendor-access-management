fa143cf8ff6a3a283fa91381c29d78169a3e167c
achraf <achrafatauil@hotmail.com>
2020-10-16 16:16:11 +0200

    Cleanup vendor/details/index controller

--- app/controllers/vendors/details/index.js
+++ app/controllers/vendors/details/index.js
@@ -6,19 +6,10 @@ export default class VendorsSubjectController extends Controller {
   page = 0;
   size = 20;
 
-  get vendor() {
-    let owner = getOwner(this);
-    return owner.lookup(`controller:vendors/details`);
-  }
-
   @action 
-  async removeFromList(bestuurseenheid){
-
-  let targetVendor = this.vendor.model
-  let relationship = this.vendor.model.canActOnBehalfOf;
-
-  relationship.removeObject(bestuurseenheid);
-  targetVendor.save();
+  async removeFromList(bestuurseenheid, vendor){
+    let relationship = await vendor.canActOnBehalfOf;
+    relationship.removeObject(bestuurseenheid);
+    vendor.save();
   }
-
 }
