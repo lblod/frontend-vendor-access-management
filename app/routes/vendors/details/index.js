644833416a44059aca896f68fd3e7b56c1f39049
achraf <achrafatauil@hotmail.com>
2020-10-16 16:18:06 +0200

    Update index route model & queryOptions

--- app/routes/vendors/details/index.js
+++ app/routes/vendors/details/index.js
@@ -2,12 +2,11 @@ import Ember from 'ember';
 import DataTableRouteMixin from 'ember-data-table/mixins/route';
 
 export default Ember.Route.extend(DataTableRouteMixin, {
-  modelName: 'bestuurseenheid',
-  
-  mergeQueryOptions(params) {
+  modelName: 'vendor',
+  mergeQueryOptions() {
     return { 
-      include: "vendors",
-     'filter[vendors][id]': this.modelFor('vendors/details').id
+      include: 'can-act-on-behalf-of',
+     'filter[id]': this.modelFor('vendors/details').id
     };
   }
 });
