bb4ea490f0ad0f04b74b196f6da7585b8c1bfe07
achraf <achrafatauil@hotmail.com>
2020-12-17 17:02:10 +0100

    upgrade syntax route vendors/index to octane

--- app/routes/vendors/index.js
+++ app/routes/vendors/index.js
@@ -1,6 +1,6 @@
 import Route from '@ember/routing/route';
 import DataTableRouteMixin from 'ember-data-table/mixins/route';
 
-export default Route.extend(DataTableRouteMixin, {
-  modelName: 'vendor'
-});
+export default class VendorsIndexRoute extends Route.extend(DataTableRouteMixin) {
+  modelName = 'vendor';
+}
