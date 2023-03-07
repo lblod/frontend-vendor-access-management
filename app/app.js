cecde74953b54ba857b5ad3218308bb520533577
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2023-03-07 10:35:17 +0100

    remove the custom-inflector-rules initializer
    We can simply import this in the app.js file since it doesn't need access to the container.

--- app/app.js
+++ app/app.js
@@ -2,6 +2,7 @@ import Application from '@ember/application';
 import Resolver from 'ember-resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from 'frontend-vendor-access-management/config/environment';
+import 'frontend-vendor-access-management/config/custom-inflector-rules';
 
 export default class App extends Application {
   modulePrefix = config.modulePrefix;
---
+++ app/config/custom-inflector-rules.js
@@ -0,0 +1,6 @@
+import Inflector from 'ember-inflector';
+
+const inflector = Inflector.inflector;
+
+inflector.irregular('bestuurseenheid', 'bestuurseenheden');
+inflector.irregular('vendor', 'vendors');
