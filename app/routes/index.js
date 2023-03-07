2375696819a9d3fc533b6318be5ea171d9930ea0
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2023-03-07 10:30:55 +0100

    Use router.transitionTo

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,7 +1,9 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
 export default class IndexRoute extends Route {
+  @service router;
   beforeModel() {
-    this.transitionTo('vendors');
+    this.router.transitionTo('vendors');
   }
 }
