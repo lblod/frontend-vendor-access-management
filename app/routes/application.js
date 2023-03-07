bcd6ebbc3930e2c7ce09faafa6fbf18a7c81b051
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2023-03-07 10:39:49 +0100

    Remove an unused tracked variable

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,12 +1,10 @@
 import Route from '@ember/routing/route';
 import { warn } from '@ember/debug';
 import { inject as service } from '@ember/service';
-import { tracked } from '@glimmer/tracking';
 
 export default class ApplicationRoute extends Route {
   @service currentSession;
   @service session;
-  @tracked error;
 
   async beforeModel() {
     await this.session.setup();
