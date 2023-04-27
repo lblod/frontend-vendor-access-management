3cce516c36d2b56dd643c198bc3f430c6a586f52
benjay10 <ben.behaeghe@redpencil.io>
2023-04-27 02:55:39 +0200

    Show formatted view only modules in data table
    When there are no view only modules, we should see some light text
    saying all modules are editable. Otherwise, we should get a nicely
    formatted list (translated and all) of the modules that are selected for
    read only.

--- app/models/bestuurseenheid.js
+++ app/models/bestuurseenheid.js
@@ -1,6 +1,10 @@
 import Model, { attr, belongsTo, hasMany } from '@ember-data/model';
+import { service } from '@ember/service';
+import { VIEW_ONLY_MODES } from '/utils/constants';
 
 export default class BestuurseenheidModel extends Model {
+  @service intl;
+
   @attr uri;
   @attr naam;
   @attr alternatieveNaam;
@@ -18,4 +22,20 @@ export default class BestuurseenheidModel extends Model {
     as: 'bestuurseenheid',
   })
   vendors;
+
+  get hasViewOnlyModules() {
+    return this.viewOnlyModules && this.viewOnlyModules.length > 0;
+  }
+
+  get formattedViewOnlyModules() {
+    if (this.viewOnlyModules)
+      return this.viewOnlyModules
+        .map((value) => {
+          for (const key in VIEW_ONLY_MODES)
+            if (VIEW_ONLY_MODES[key] === value) return key;
+        })
+        .map((key) => this.intl.t('vendor.subject.modules.' + key))
+        .join(', ');
+    else return '';
+  }
 }
