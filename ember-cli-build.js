ecdcbd5336d81533ed58d7ae588154e533eed665
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2025-06-04 16:19:00 +0200

    Update to Appuniversum v3

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,9 +4,6 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function (defaults) {
   const app = new EmberApp(defaults, {
-    '@appuniversum/ember-appuniversum': {
-      disableWormholeElement: true,
-    },
     // Add options here
   });
 
