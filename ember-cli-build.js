3d274111917af29e843386e25950b10304f8287d
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2025-06-04 17:04:25 +0200

    Resolve some Appuniversum v3 deprecations

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,7 +4,13 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function (defaults) {
   const app = new EmberApp(defaults, {
-    // Add options here
+    '@embroider/macros': {
+      setConfig: {
+        '@appuniversum/ember-appuniversum': {
+          disableInternalAuContentUsage: true,
+        },
+      },
+    },
   });
 
   const { Webpack } = require('@embroider/webpack');
