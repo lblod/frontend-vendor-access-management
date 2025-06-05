6f90aafc67a05a1e5e38ad399cb37a6b9adbe8bc
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2025-06-05 11:56:16 +0200

    Update to Ember 6.4 LTS

--- app/app.js
+++ app/app.js
@@ -3,6 +3,11 @@ import Resolver from 'ember-resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from 'frontend-vendor-access-management/config/environment';
 import 'frontend-vendor-access-management/config/custom-inflector-rules';
+import { importSync, isDevelopingApp, macroCondition } from '@embroider/macros';
+
+if (macroCondition(isDevelopingApp())) {
+  importSync('./deprecation-workflow');
+}
 
 export default class App extends Application {
   modulePrefix = config.modulePrefix;
--- ember-cli-build.js
+++ ember-cli-build.js
@@ -18,6 +18,10 @@ module.exports = function (defaults) {
     },
     emberData: {
       deprecations: {
+        // New projects can safely leave this deprecation disabled.
+        // If upgrading, to opt-into the deprecated behavior, set this to true and then follow:
+        // https://deprecations.emberjs.com/id/ember-data-deprecate-store-extends-ember-object
+        // before upgrading to Ember Data 6.0
         DEPRECATE_STORE_EXTENDS_EMBER_OBJECT: false,
       },
     },
@@ -27,10 +31,8 @@ module.exports = function (defaults) {
   return require('@embroider/compat').compatBuild(app, Webpack, {
     staticAddonTestSupportTrees: true,
     staticAddonTrees: true,
-    staticHelpers: true,
-    staticModifiers: true,
-    staticComponents: true,
     staticEmberSource: true,
+    staticInvokables: true,
     skipBabel: [
       {
         package: 'qunit',
